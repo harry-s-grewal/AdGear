728e9e482060f3778974143c6810a495abed8bc6
Harry Grewal <46252218+harry-s-grewal@users.noreply.github.com>
2021-04-11 20:08:36 -0400

    Update query_db.py

--- query_db.py
+++ query_db.py
@@ -20,6 +20,7 @@ counts = dict((key, 0) for key in sys.argv[1:])
 with open("db.json") as f:
     db_json = json.load(f)
 
+# ThreadPoolExecuter automatically allocates future jobs to threads. The number of threads specified here is 3
 with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
     future_count = {executor.submit(count_key, key): key for key in counts}
     for future in concurrent.futures.as_completed(future_count):
